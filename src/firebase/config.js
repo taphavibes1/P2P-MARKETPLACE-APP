40b679d71a846430515eaf48aa1acdab4c146c74
Zubair Muhammed Mustapha Amoto <mustyapha69@gmail.com>
2026-05-19 12:14:26 +0100

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -6,7 +6,46 @@ import { getStorage } from 'firebase/storage';
 // Replace with your Firebase project config
 const firebaseConfig = {
   apiKey: "FIREBASE_API_KEY",
-  authDomain: "FIREBASE_AUTH_DOMAIN",
+ 
+  authDomain: "{
+  "project_info": {
+    "project_number": "1034362432289",
+    "project_id": "p2p-marketplace-92895",
+    "storage_bucket": "p2p-marketplace-92895.firebasestorage.app"
+  },
+  "client": [
+    {
+      "client_info": {
+        "mobilesdk_app_id": "1:1034362432289:android:296321588887de27e06c60",
+        "android_client_info": {
+          "package_name": "com.twmcreated.CompusMarketApp"
+        }
+      },
+      "oauth_client": [
+        {
+          "client_id": "1034362432289-ro0crh1pksqu2m720q2mgud449uajg12.apps.googleusercontent.com",
+          "client_type": 3
+        }
+      ],
+      "api_key": [
+        {
+          "current_key": "AIzaSyA1TFHSoYhbKtIhDbUysLA2Gu5un0kEtws"
+        }
+      ],
+      "services": {
+        "appinvite_service": {
+          "other_platform_oauth_client": [
+            {
+              "client_id": "1034362432289-ro0crh1pksqu2m720q2mgud449uajg12.apps.googleusercontent.com",
+              "client_type": 3
+            }
+          ]
+        }
+      }
+    }
+  ],
+  "configuration_version": "1"
+}",
   projectId: "FIREBASE_PROJECT_ID",
   storageBucket: "FIREBASE_STORAGE_BUCKET",
   messagingSenderId: "FIREBASE_MESSAGING_SENDER_ID",
